aa72d7f08493167bfb6322d5553f114f5995b363
Carlos Campos <111520257+CarlosMiguelAzevedoCampos@users.noreply.github.com>
2025-07-09 23:06:24 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,8 +3,8 @@ module "security_account_st3633969" {
 
   control_tower_parameters = {
     AccountEmail             = "st3633969@gmail.com"
-    AccountName              = "security-account-st3633969"
-    ManagedOrganizationalUnit = "Security"
+    AccountName              = "networking-account-st3633969"
+    ManagedOrganizationalUnit = "Networking"
     SSOUserEmail             = "st3633969@gmail.com"
     SSOUserFirstName         = "ST"
     SSOUserLastName          = "3633969"
@@ -12,24 +12,24 @@ module "security_account_st3633969" {
 
   account_tags = {
     "ABC:Owner"       = "st3633969@gmail.com"
-    "ABC:Division"    = "Security"
+    "ABC:Division"    = "Networking"
     "ABC:Environment" = "Prod"
     "ABC:CostCenter"  = "SEC123"
     "ABC:Vended"      = "true"
     "ABC:DivCode"     = "SEC"
     "ABC:BUCode"      = "SEC001"
-    "ABC:Project"     = "SecurityOnboarding"
+    "ABC:Project"     = "Networking"
   }
 
   change_management_parameters = {
     change_requested_by = "ST 3633969"
-    change_reason       = "Provisioning security OU account for production use"
+    change_reason       = "Provisioning Networking OU account for production use"
   }
 
   custom_fields = {
-    custom1 = "security"
+    custom1 = "Networking"
     custom2 = "production"
   }
 
-  account_customizations_name = "security-customizations"
+  account_customizations_name = "networking-customizations"
 }
