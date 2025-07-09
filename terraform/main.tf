8600f61001dc0b21205bb241d446d2d45e6301df
Carlos Campos <111520257+CarlosMiguelAzevedoCampos@users.noreply.github.com>
2025-07-09 15:42:19 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,41 @@
-module "sandbox" {
+# Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+module "sandbox_account_01" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "carlosmiguel.sandbox.1996@gmail.com"
-    AccountName               = "sandbox-aft"
+    AccountEmail = "carlosmiguel.sandbox.1996@gmail.com"
+    AccountName  = "sandbox-account-01"
+    # Syntax for top-level OU
     ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "carlosmiguel.sandbox.1996@gmail.com"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    # Syntax for nested OU
+    # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
+    SSOUserEmail     = "carlosmiguel.sandbox.1996@gmail.com"
+    SSOUserFirstName = "John"
+    SSOUserLastName  = "Doe"
   }
 
   account_tags = {
-    "Learn Tutorial" = "AFT"
+    "ABC:Owner"       = "john.doe@amazon.com"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Dev"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
   }
 
   change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+    change_requested_by = "John Doe"
+    change_reason       = "testing the account vending process"
   }
 
   custom_fields = {
-    group = "non-prod"
+    custom1 = "a"
+    custom2 = "b"
   }
 
-  account_customizations_name = "sandbox"
+  account_customizations_name = "sandbox-customizations"
 }
